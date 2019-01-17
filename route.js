c16ede90623cdf91f50f7f3a8795da152cad0a32
Richard Morrell Hoeata <riki@craftsmoothie.co.nz>
2019-01-18 12:33:30 +1300

    commenting out the answer route for testing

--- route.js
+++ route.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const route = express.Router()
+//const functions = require('./functions')
 
 module.exports = route
 
 route.get('/ed8ball', (req, res) => {
-  res.send('Home route is working')
+  // res.send('Home route is working')
+  res.render('question')
 })
 
-route.get('/answers', (req, res) => {
-  res.send('This is the answer page')
-})
+// route.get('/answers', (req, res) => {
+//   //res.send('This is the answer page')
+
+//   let randomAns = functions.getRandomAns()
+//   let data = {
+//     answer: randomAns
+//   }
+//   res.render('answer', data)
+// })
