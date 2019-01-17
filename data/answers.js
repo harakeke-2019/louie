779593314822108432bceda4b1043b4e2cae8598
fetuolemoana <fetu.tamapeau@gmail.com>
2019-01-18 10:21:05 +1300

    Add answers data

---
+++ data/answers.js
@@ -0,0 +1,82 @@
+answers = [
+  {
+    id: 1, 
+    answer: 'It is certain'
+  }, 
+  {
+    id: 2, 
+    answer: 'As I see it, yes'
+  }, 
+  {
+    id: 3,
+    answer: 'It is decidedly so'
+  },
+  {
+    id: 4,
+    answer: 'Without a doubt'
+  },
+  {
+    id: 5,
+    answer: 'Yes definitely'
+  },
+  {
+    id: 6,
+    answer: 'You may rely on it'
+  },
+  {
+    id: 7,
+    answer: 'As I see it yes'
+  },
+  {
+    id: 8,
+    answer: 'Most likely'
+  },
+  {
+    id: 9,
+    answer: 'Outlook good'
+  },
+  {
+    id: 10,
+    answer: 'Yes'
+  },
+  {
+    id: 11,
+    answer: 'Signs point to yes'
+  },
+  {
+    id: 12,
+    answer: 'Reply hazy try again'
+  },
+  {
+    id: 13,
+    answer: 'Ask again later'
+  },
+  {
+    id: 14,
+    answer: 'Better not tell you now'
+  },
+  {
+    id: 15,
+    answer: 'Cannot predict now'
+  },
+  {
+    id: 16,
+    answer: 'Concentrate and ask again'
+  },
+  {
+    id: 17,
+    answer: 'Don\'t count on it' 
+  },
+  {
+    id: 18,
+    answer: 'My reply is no'
+  },
+  {
+    id: 19,
+    answer: 'My sauces say no'
+  },
+  {
+    id: 20,
+    answer: 'Outlook not so good'
+  },
+]
