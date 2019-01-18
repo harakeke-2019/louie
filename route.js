63a1772cd276ac367e3eb61c7f37a495839d3f7f
nzbenji <benlittle53@gmail.com>
2019-01-18 16:00:04 +1300

    change on routes

--- route.js
+++ route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const route = express.Router()
-//const functions = require('./functions')
+const functions = ('./functions')
+const getRandomAnswer = functions.anchor
 
 module.exports = route
 
@@ -9,12 +10,16 @@ route.get('/ed8ball', (req, res) => {
   res.render('question')
 })
 
-// route.get('/answers', (req, res) => {
-//   //res.send('This is the answer page')
-
-//   let randomAns = functions.getRandomAns()
-//   let data = {
-//     answer: randomAns
-//   }
-//   res.render('answer', data)
-// })
+route.get('/answers', (req, res) => {
+  // res.send('This is the answer page')
+  getRandomAnswer((err, answerFile) => {
+    if (err) throw err
+    let randomId = parseInt(Math.random() * 20)
+    let answers = answerFile.randomAns
+    let outputAns = answers.find(answer => answer.id == randomId)
+    let data = {
+      answer: outputAns.answer
+    }
+    res.render('answer', data)
+  })
+})
